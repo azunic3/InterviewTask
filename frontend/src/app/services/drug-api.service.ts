9fdec191546110115ba9c140c6e2e131065d8ec6
azunic3 <azunic3@etf.unsa.ba>
2026-02-06 19:26:08 +0100

    fixed url

--- frontend/src/app/services/drug-api.service.ts
+++ frontend/src/app/services/drug-api.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 export interface PopularSearch {
   query: string;
@@ -12,8 +13,7 @@ export interface PopularSearch {
 })
 export class DrugApiService {
   // backend base URL
-
-  private readonly baseUrl = '${environment.apiBaseUrl}/api';
+  private baseUrl = environment.apiBaseUrl;
 
   constructor(private http: HttpClient) {}
 
